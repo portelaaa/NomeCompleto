1aca9e3fbf070cf524029c824e35638c62a564c4
portelaaa <e107ti@etec.sp.gov.br>
2023-08-29 20:09:19 -0300

    Exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Digite seu primeiro nome");
+string nome;
+nome = Console.ReadLine()!;
+
+Console.WriteLine("Digite seu sobrenome");
+string sobrenome;
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome de catálago: {sobrenome.ToUpper()}, {nome}");
